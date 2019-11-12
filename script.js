9694d6af9c30e4aa8b7077ff0b80d8be5df53028
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-11-12 13:00:34 +0100

    using parent node data index

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ span.setAttribute("id", "currDateTime");
 
 appCont.append(span);
 
+const START = 9;
+const END = 17;
 let hours = [
   "9 AM",
   "10 AM",
@@ -27,10 +29,12 @@ let hours = [
   "4 PM",
   "5 PM"
 ];
-
+var activities;
 if (localStorage.getItem("activities") == null) {
-  var activities = ["", "", "", "", "", "", "", "", ""];
-} else activities = JSON.parse(localStorage.getItem("activities"));
+  activities = ["", "", "", "", "", "", "", "", ""];
+} else {
+  activities = JSON.parse(localStorage.getItem("activities"))
+};
 
 let tbody = document.createElement("div");
 
@@ -74,13 +78,14 @@ for (let index = 0; index < hours.length; index++) {
   tdInputField.textContent = activities[index];
 
   tr.append(tdInputField);
+  tr.setAttribute("data-index", index);
 
   let tdSaveButton = document.createElement("button");
   tdSaveButton.classList.add("saveButton", "col-2");
-  tdSaveButton.setAttribute("data-index", index);
+  
 
   tdSaveButton.textContent = "Save";
-  tdSaveButton.setAttribute("data-state", "save");
+  //tdSaveButton.setAttribute("data-state", "save");
 
   tdSaveButton.classList.add("btn", "btn-primary", "border");
 
@@ -97,8 +102,8 @@ app.append(appCont);
 
 document.addEventListener("click", event => {
   if (event.target.nodeName === "BUTTON") {
-    let index = event.target.getAttribute("data-index");
-    let input = document.querySelector("textarea[data-index='" + index + "']")
+    let index = event.target.parentNode.getAttribute("data-index");
+    let input = event.target.parentNode.querySelector("textarea")
       .value;
     if (activities[index] === input) {
       console.log(input);
